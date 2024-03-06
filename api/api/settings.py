5a80a67ecfd724996ab12a299186c9971f5bb9fb
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-03-06 20:04:35 -0300

    :bricks: ci: Base para o docker

--- api/api/settings.py
+++ api/api/settings.py
@@ -62,6 +62,11 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'briefing',
+                'category',
+                'retailer',
+                'vendor'
+                'rest_framework'
             ],
         },
     },

9578350f1f103df1c8c60e117907465f004abcc3
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-03-06 21:49:16 -0300

    :sparkles: feat: Model e serializer do category

--- api/category/models.py
+++ api/category/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    class Meta:
+
+        db_table = 'category'
+
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name

d11d69420eaaadbebc89958c54fee39e2b9474f2
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-03-06 21:49:07 -0300

    :sparkles: feat: Model e serializer do briefing

--- api/briefing/models.py
+++ api/briefing/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Briefing(models.Model):
+    class Meta:
+
+        db_table = 'briefing'
+
+    name = models.CharField(max_length=200)
+    retailer = models.CharField(max_length=200)
+    responsible = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
+    release_date = models.CharField(max_length=200)
+    availabe = models.IntegerField()
+
+    def __str__(self):
+        return self.name

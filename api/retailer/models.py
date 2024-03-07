2729866f66cbbae8f54fe92b4a921ad3f716e734
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-03-06 21:49:26 -0300

    :sparkles: feat: Model e serializer do retailer

--- api/retailer/models.py
+++ api/retailer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Retailer(models.Model):
+    class Meta:
+
+        db_table = 'retailer'
+
+    name = models.CharField(max_length=200)
+    vendors = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name

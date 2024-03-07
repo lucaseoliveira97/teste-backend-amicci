755018cace6a60fdc3049c754d2cec99e4181ac6
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-03-06 21:49:33 -0300

    :sparkles: feat: Model e serializer do vendor

--- api/vendor/models.py
+++ api/vendor/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Vendor(models.Model):
+    class Meta:
+
+        db_table = 'vendor'
+
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name

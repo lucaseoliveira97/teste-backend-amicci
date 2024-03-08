bc7beda06be309cb6387b3d5dbcc28fdc6f2430a
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-03-08 11:18:59 -0300

    :sparkles: feat: Criando rotas de categorias

--- api/category/views.py
+++ api/category/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+class CategoriesListView (generics.ListAPIView):
+    queryset=Category.objects.all()
+    serializer_class=CategorySerializer
+
+class CategoryCreateView (generics.CreateAPIView):
+    queryset=Category.objects.all()
+    serializer_class=CategorySerializer
+
+class CategoryRetrieveUpdateView (generics.RetrieveUpdateAPIView):
+    queryset=Category.objects.all()
+    serializer_class=CategorySerializer
+
+

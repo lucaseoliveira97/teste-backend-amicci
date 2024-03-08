fbfcc5835b9cf66faefeebc8372abe92e20acd0a
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-03-08 18:05:04 -0300

    :sparkles: feat: Criando relacionamento da tabela de briefings

--- api/briefing/models.py
+++ api/briefing/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from retailer.models import Retailer
 
 from category.models import Category
 
@@ -8,7 +9,7 @@ class Briefing(models.Model):
         db_table = 'briefing'
 
     name = models.CharField(max_length=200)
-    retailer = models.CharField(max_length=200)
+    retailer = models.ForeignKey(Retailer, related_name="retailer", on_delete=models.CASCADE)
     responsible = models.CharField(max_length=200)
     category = models.ForeignKey(Category, related_name="category", on_delete=models.CASCADE)
     release_date = models.CharField(max_length=200)
